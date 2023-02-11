1ab4a0493403e38433463ab675137c4fb2eaa17d
Marto Nieto Guerrero <marto.nieto.g16@gmail.com>
2023-02-11 11:00:00 -0500

    Update Token_BEP20.sol

--- contract/Token_BEP20.sol
+++ contract/Token_BEP20.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TorCoin is ERC20 {
+contract BEP20 is ERC20 {
 	constructor() ERC20 ("Name", "Symbol"){
-	    _mint(address(this), 50000000 * (10 ** uint256(decimals())));
+	    _mint(address(this), 1000000000 * (10 ** uint256(decimals())));
 	    _approve(address(this), msg.sender, totalSupply());
 	    _transfer(address(this), msg.sender, totalSupply());
 	}
